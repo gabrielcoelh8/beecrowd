56f94b2b7f9ba0b0225ec1a5ae82ae44b9085cba
Gabriel Coelho <gabrielroberto099@gmail.com>
2023-02-14 14:53:46 -0400

    bee1075

---
+++ iniciante/bee1075.java
@@ -0,0 +1,14 @@
+package beecrowd.iniciante;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gabrielcoelho
+ */
+public class bee1075 {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+    }
+}
